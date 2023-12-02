b173f17e4f4188cdc0db85bbec5eb0fdf2b1f44a
serprointec <guamanzh@gmail.com>
2023-12-02 11:41:42 -0500

    Codigo_Practicas

---
+++ Codigo_Practicas.sql
@@ -0,0 +1,9 @@
+/*Crear Una tabla Estudiante*/
+
+CREATE TABLE Estudiante (
+ID INT PRIMARY KEY,
+Nombre VARCHAR(100) NOT NULL,
+Apellido VARCHAR(100) NOT NULL,
+Edad INT,
+CorreoElectronico VARCHAR(100)
+);
