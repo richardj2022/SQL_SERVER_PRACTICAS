19a5307f86f3aa5694baed03947f40f72263e3f4
serprointec <guamanzh@gmail.com>
2024-05-16 22:37:31 -0500

    create table Usuarios

---
+++ create table Usuarios.sql
@@ -0,0 +1,8 @@
+-- Creacion de tabla una tabla --
+create table Usuarios (
+-- NOMBRECAMPO TIPODATO,
+IdUser int primary key,
+NombreUser varchar(50),
+Usuario varchar(50),
+Userpassword varchar(50)
+);
